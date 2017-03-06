8a19ae7592087f48cc980729ca84d44ee4a9f55d
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-03-06 15:35:51 +0100

    Extracting data from post using viewmodel.

---
+++ src/FrontendMVC/ViewModels/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontendMVC.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password1 { get; set; }
+        public string Password2 { get; set; }
+    }
+}
