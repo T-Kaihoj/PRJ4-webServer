d1a15844483f0210ca1eb68fe94fec1ad91f8734
Niklas <niklasmms7@hotmail.com>
2017-03-06 14:52:48 +0100

    Worked on the datepicker (in progress)

--- src/FrontendMVC/wwwroot/_references.js
+++ src/FrontendMVC/wwwroot/_references.js
@@ -4,5 +4,6 @@
 /// <reference path="lib/bootstrap-material-design/dist/js/material.js" />
 /// <reference path="lib/bootstrap-material-design/dist/js/ripples.js" />
 /// <reference path="lib/bootstrap-sass/assets/javascripts/bootstrap.js" />
+/// <reference path="lib/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/moment/moment.js" />
