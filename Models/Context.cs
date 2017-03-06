f5ca59a24fffe88a9f11fccab167c36d2c1ddaba
HootERasmus <kominumsen@gmail.com>
2017-03-06 18:06:24 +0100

    Datamodel

---
+++ Models/Context.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models.Model;
+
+namespace Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Bet> Bets { get; set; }
+        public DbSet<Lobby> Lobbies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LocalBet;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        
+        /*
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbUpdateException e)
+            {
+                foreach (var eve in e.InnerException)
+                {
+                    Debug.WriteLine(@"Entity of type \"{ 0}\" in state \"{1}\" has the
+                                     following validation errors: ",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Debug.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
+            return 0;
+        }
+        */
+    }
+}
---
+++ src/Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models;
+using Models.Model;
+
+namespace Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (Context db = new Context())
+            {
+             
+            var bruger = new User
+            {
+                Balance = 100,
+                Email = "dfd@dkfdf.com",
+                FirstName = "Jeppe",
+                Hash = "dsfsfsf",
+                LastName = "Traberg",
+                Salt = "fsfsf",
+                Username = "fdsfdfsdff"
+
+            };
+
+                db.Users.Add(bruger);
+                
+                db.SaveChanges();
+                
+            }
+        }
+    }
+}
