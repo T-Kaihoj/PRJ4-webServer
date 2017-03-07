01fa8d311da3f08f813a4c6b60d0fa57db0b856f
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-03-07 10:36:01 +0100

    Minor edits to user.

--- src/FrontendMVC/Controllers/UserController.cs
+++ src/FrontendMVC/Controllers/UserController.cs
@@ -9,10 +9,47 @@ namespace FrontendMVC.Controllers
 {
     public class UserController : Controller
     {
-        
-        // POST: /UserController/
+        // GET: /UserController/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // TODO: Get the user from the identity.
+            var user = new UserViewModel();
+            // TODO: Get the user in repository.
+
+            return View("Profile", user);
+        }
+
+        // GET: /UserController/
+        /*[HttpGet]
+        public IActionResult Get(string userName)
+        {
+            // TODO: Get the user from the identity.
+            var user = new UserViewModel();
+            // TODO: Get the user in repository.
+
+            return View("Profile", user);
+        }*/
+
+        // PATCH: /UserController/
+        [HttpPatch]
+        public IActionResult Patch(UserViewModel model)
+        {
+            // Validate the model.
+            if (!TryValidateModel(model))
+            {
+                // Error, return to main page with the model.
+                return View("Profile", model);
+            }
+
+            // TODO: Persist user in repository.
+
+            return Redirect("~");
+        }
+
+        // POST: /UserController/CreateUser
         [HttpPost]
-        public IActionResult Post(UserViewModel model)
+        public IActionResult CreateUser(UserViewModel model)
         {
             // Validate the model.
             if (!TryValidateModel(model))
@@ -21,8 +58,11 @@ namespace FrontendMVC.Controllers
                 return View("~/Views/Home/Index.cshtml", model);
             }
 
-            model.UserName = "Tak -- Tak";
-            return View("~/Views/Home/Index.cshtml",model);
+            // TODO: Check if username is in use.
+
+            // TODO: Persist user in repository.
+
+            return View("UserCreated");
         }
     }
 }
