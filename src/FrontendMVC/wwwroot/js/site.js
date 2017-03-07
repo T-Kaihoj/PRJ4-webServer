c9a79654bd289c7fdcaa07a8c40af38453f1e621
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-03-07 12:00:07 +0100

    Frontend validation.

--- src/FrontendMVC/wwwroot/js/site.js
+++ src/FrontendMVC/wwwroot/js/site.js
@@ -1,2 +1,21 @@
-﻿// Write your Javascript code.
+﻿// Initialize Material Design.
 $.material.init();
+
+// Setup jQuery Validate for Bootstrap integration.
+jQuery.validator.setDefaults({
+    highlight: function (element) {
+        jQuery(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function (element) {
+        jQuery(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'label label-danger',
+    errorPlacement: function (error, element) {
+        if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
