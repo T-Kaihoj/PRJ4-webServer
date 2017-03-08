af9ab8912c51c940df934ff1d29170ac25d7c0ee
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-03-08 08:48:54 +0100

    Fix for JavaScript being executed too early.

--- src/FrontendMVC/wwwroot/js/site.js
+++ src/FrontendMVC/wwwroot/js/site.js
@@ -1,21 +1,24 @@
-﻿// Initialize Material Design.
-$.material.init();
+﻿// Wait for jQuery to load. Done after the DOM is loaded.
+$(document).ready(function () {
+    // Initialize Material Design.
+    $.material.init();
 
-// Setup jQuery Validate for Bootstrap integration.
-jQuery.validator.setDefaults({
-    highlight: function (element) {
-        jQuery(element).closest('.form-group').addClass('has-error');
-    },
-    unhighlight: function (element) {
-        jQuery(element).closest('.form-group').removeClass('has-error');
-    },
-    errorElement: 'span',
-    errorClass: 'label label-danger',
-    errorPlacement: function (error, element) {
-        if (element.parent('.input-group').length) {
-            error.insertAfter(element.parent());
-        } else {
-            error.insertAfter(element);
+    // Setup jQuery Validate for Bootstrap integration.
+    jQuery.validator.setDefaults({
+        highlight: function (element) {
+            jQuery(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function (element) {
+            jQuery(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'label label-danger',
+        errorPlacement: function (error, element) {
+            if (element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
         }
-    }
+    });
 });
