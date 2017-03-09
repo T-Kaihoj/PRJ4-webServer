e8446e40a4ded0185aba0fc05a20334400ce20ea
Dan Lee <au531980@uni.au.dk>
2017-03-09 14:44:49 +0100

    dan business

---
+++ src/FrontendMVC/Business/UserLogic/ICreateUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontendMVC.Business.UserLogic
+{
+    public interface ICreateUser
+    {
+        bool createUser(string firstName, string lastName, string email);
+    }
+}
