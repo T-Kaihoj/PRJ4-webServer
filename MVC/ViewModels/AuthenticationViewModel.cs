c188e4f5aee8c1e63aa53885dee164c268fdcee3
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-03-13 14:42:25 +0100

    Move viewmodels and rename namespace.

--- MVC/ViewModels/AuthenticationViewModel.cs
+++ MVC/ViewModels/AuthenticationViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FrontendMVC.ViewModels
+namespace MVC.ViewModels
 {
     public class AuthenticationViewModel
     {
--- MVC/ViewModels/CreateLobbyViewModel.cs
+++ MVC/ViewModels/CreateLobbyViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FrontendMVC.ViewModels
+namespace MVC.ViewModels
 {
     public class CreateLobbyViewModel
     {
