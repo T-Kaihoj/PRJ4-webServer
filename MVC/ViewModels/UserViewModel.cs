8803e3f892013e0ccf76c1470b096b89906840aa
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-03-13 17:01:20 +0100

    Add trimming to make tests pass.

--- MVC/ViewModels/UserViewModel.cs
+++ MVC/ViewModels/UserViewModel.cs
@@ -1,31 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MVC.ViewModels
 {
     public class UserViewModel
     {
-        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorFirstNameRequired")]
-        public string FirstName { get; set; } = String.Empty;
+        // Private backing fields.
+        private string _email = string.Empty;
+        private string _firstName = string.Empty;
+        private string _lastName = string.Empty;
+        private string _password1 = string.Empty;
+        private string _password2 = string.Empty;
+        private string _userName = string.Empty;
 
-        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorLastNameRequired")]
-        public string LastName { get; set; } = String.Empty;
+        #region Accessors.
 
-        [EmailAddress(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorEmailInvalid")]
-        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorEmailRequired")]
-        public string Email { get; set; } = String.Empty;
+        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors),
+            ErrorMessageResourceName = "ErrorFirstNameRequired")]
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value.Trim();
+            }
+        }
 
-        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorUserNameRequired")]
-        public string UserName { get; set; } = String.Empty;
+        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors),
+            ErrorMessageResourceName = "ErrorLastNameRequired")]
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value.Trim();
+            }
+        }
 
-        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorPasswordRequired1")]
-        public string Password1 { get; set; } = String.Empty;
+        [EmailAddress(ErrorMessageResourceType = typeof(UserViewModelErrors),
+            ErrorMessageResourceName = "ErrorEmailInvalid")]
+        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors),
+            ErrorMessageResourceName = "ErrorEmailRequired")]
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value.Trim();
+            }
+        }
 
-        [Compare("Password1", ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorPasswordNotIdentical")]
-        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorPasswordRequired2")]
-        public string Password2 { get; set; } = String.Empty;
+        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors),
+            ErrorMessageResourceName = "ErrorUserNameRequired")]
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                _userName = value.Trim();
+            }
+        }
+
+        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors),
+            ErrorMessageResourceName = "ErrorPasswordRequired1")]
+        public string Password1
+        {
+            get
+            {
+                return _password1;
+            }
+            set
+            {
+                _password1 = value.Trim();
+            }
+        }
+
+        [Compare("Password1",
+            ErrorMessageResourceType = typeof(UserViewModelErrors),
+            ErrorMessageResourceName = "ErrorPasswordNotIdentical")]
+        [Required(ErrorMessageResourceType = typeof(UserViewModelErrors),
+            ErrorMessageResourceName = "ErrorPasswordRequired2")]
+        public string Password2
+        {
+            get
+            {
+                return _password2;
+            }
+            set
+            {
+                _password2 = value.Trim();
+            }
+        }
+
+        #endregion
     }
 }
