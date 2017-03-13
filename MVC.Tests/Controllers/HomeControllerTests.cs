6b700d5e46ccedfe8302731b8a52315fb351d5d6
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-03-13 16:54:14 +0100

    Add basic tests and default values to viewmodel.

---
+++ MVC.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+using MVC.Controllers;
+using MVC.ViewModels;
+using NUnit.Framework;
+
+namespace MVC.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private HomeController uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new HomeController();
+        }
+
+        [Test]
+        public void Index_WithNoInput_ReturnsExpectedResult()
+        {
+            // Act.
+            var result = uut.Index();
+
+            // Assert that we got the right result type.
+            Assert.That(result, Is.TypeOf<ViewResult>());
+
+            // Continue testing on the result.
+            var vResult = result as ViewResult;
+
+            // Check that the viewmodel is correct.
+            Assert.That(vResult.ViewData.Model, Is.TypeOf<UserViewModel>());
+
+            // Continue testing on the model.
+            var model = vResult.ViewData.Model as UserViewModel;
+
+            // All fields should be empty.
+            Assert.That(model.Email, Is.Empty);
+            Assert.That(model.FirstName, Is.Empty);
+            Assert.That(model.LastName, Is.Empty);
+            Assert.That(model.Password1, Is.Empty);
+            Assert.That(model.Password2, Is.Empty);
+            Assert.That(model.UserName, Is.Empty);
+        }
+    }
+}
--- MVC/ViewModels/UserViewModel.cs
+++ MVC/ViewModels/UserViewModel.cs
@@ -9,23 +9,23 @@ namespace MVC.ViewModels
     public class UserViewModel
     {
         [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorFirstNameRequired")]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = String.Empty;
 
         [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorLastNameRequired")]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = String.Empty;
 
         [EmailAddress(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorEmailInvalid")]
         [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorEmailRequired")]
-        public string Email { get; set; }
+        public string Email { get; set; } = String.Empty;
 
         [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorUserNameRequired")]
-        public string UserName { get; set; }
+        public string UserName { get; set; } = String.Empty;
 
         [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorPasswordRequired1")]
-        public string Password1 { get; set; }
+        public string Password1 { get; set; } = String.Empty;
 
         [Compare("Password1", ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorPasswordNotIdentical")]
         [Required(ErrorMessageResourceType = typeof(UserViewModelErrors), ErrorMessageResourceName = "ErrorPasswordRequired2")]
-        public string Password2 { get; set; }
+        public string Password2 { get; set; } = String.Empty;
     }
 }
