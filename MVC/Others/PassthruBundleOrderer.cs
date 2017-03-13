77c76b041b7c9d08407e33d0e9ebfabcf01ea13e
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-03-13 14:39:24 +0100

    Add reference for source.

--- MVC/Others/PassthruBundleOrderer.cs
+++ MVC/Others/PassthruBundleOrderer.cs
@@ -3,6 +3,7 @@ using System.Web.Optimization;
 
 namespace MVC
 {
+    // See https://stevescodingblog.co.uk/changing-the-ordering-for-single-bundles-in-asp-net-4/
     class PassthruBundleOrderer : IBundleOrderer
     {
         public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
