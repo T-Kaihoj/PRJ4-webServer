3de120834a7264d5e489279bfd614e5586672364
Dan Lee <au531980@uni.au.dk>
2017-03-14 15:09:33 +0100

    Business UserLogic

---
+++ MVC/Models/Userlogic/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserLogic
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
