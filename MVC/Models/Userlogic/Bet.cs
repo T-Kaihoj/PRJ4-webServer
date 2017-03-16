3102f7b9f48efe90e56fca4342fb5a604022b731
Dan Lee <au531980@uni.au.dk>
2017-03-16 12:59:20 +0100

    Lobby og Bet interfaces (hurtigt lavet)

---
+++ MVC/Models/Userlogic/Bet.cs
@@ -0,0 +1,13 @@
+﻿namespace BetLogic
+{
+    public class Bet
+    {
+
+        public string BetID { get; set; }
+        public string BetName { get; set; }
+        public string Describtion { get; set; }
+        public User Judge { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
---
+++ MVC/Models/Userlogic/Lobby.cs
@@ -0,0 +1,17 @@
+﻿using BetLogic;
+using UserLogic;
+
+
+namespace LobbyLogic
+{
+    public class Lobby
+    {
+        public long LobbyID { get; set; }
+        public string Describtion { get; set; }
+        public string LobbyName { get; set; }
+        public List<User> Participants { get; set; }
+        //Måske laves om til en liste af BetID'er?
+        public List<Bet> Bets { get; set; }
+
+    }
+}
