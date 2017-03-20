4260b14e8803acf6f8f543a7833772ad4e3d2b7f
Niklas <niklasmms7@hotmail.com>
2017-03-20 13:48:34 +0100

    Added Lobbies view, added Lobbies to menu. Changes to LobbyController.cs

---
+++ MVC/Controllers/LobbiesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class LobbiesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
