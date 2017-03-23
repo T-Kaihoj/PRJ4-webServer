dec59fd8e7b996b059f52649d29e60d036405f0b
Jeppe Traberg <J.TrabergS@gmail.com>
2017-03-23 09:28:55 +0100

    Mergede to tests i UserRepository tests der testede det samme. Tilføjet unittestfiler til det andre repos

--- DAL.Tests/UserRepositoryTests.cs
+++ DAL.Tests/UserRepositoryTests.cs
@@ -44,30 +44,27 @@ namespace DAL.Tests
         [Test]
         public void Get_InsertedPersonIsRetrieved_BothPersonsIdentical()
         {
-            // Create a new user.
-            var user1 = new User()
+            string username = "The_Killer";
+
+            var user = new User()
             {
-                Username = "The_KilL3rrrr",
+                Username = username,
                 Outcomes = null,
                 InvitedToLobbies = null,
                 FirstName = "Jeppe",
                 MemberOfLobbies = null,
                 Balance = 50,
                 Bets = null,
-                Email = "J.TrabergS@gmail.com",
+                Email = "fsdfff@dfdfdf.com",
                 Hash = "sdkjfldfkdf",
                 Salt = "dsfdfsfdsfsfd",
                 LastName = "Soerensen"
             };
 
-            // Add user to database
-            _uut.Add(user1);
+            _uut.Add(user);
             _context.SaveChanges();
 
-            // Retrieve user from database
-            var user2 = _uut.Get(user1.Username);
-
-            Assert.That(user1, Is.EqualTo(user2));
+            Assert.That(_uut.Get(username), Is.EqualTo(user));
         }
 
         [Test]
@@ -158,34 +155,9 @@ namespace DAL.Tests
             Assert.That(() => _context.SaveChanges(), Throws.Exception);
         }
 
-        [Test]
-        public void Get_RetrievesOneUser_UserFound()
-        {
-            string username = "The_Killer";
-
-            var user = new User()
-            {
-                Username = username,
-                Outcomes = null,
-                InvitedToLobbies = null,
-                FirstName = "Jeppe",
-                MemberOfLobbies = null,
-                Balance = 50,
-                Bets = null,
-                Email = "fsdfff@dfdfdf.com",
-                Hash = "sdkjfldfkdf",
-                Salt = "dsfdfsfdsfsfd",
-                LastName = "Soerensen"
-            };
-
-            _uut.Add(user);
-            _context.SaveChanges();
-
-            Assert.That(_uut.Get(username), Is.EqualTo(user));
-        }
 
         [Test]
-        public void Get_TryReceiveUserNotInDatabase_ThrowsException()
+        public void Get_TryReceiveUserNotInDatabase_IsNull()
         {
             Assert.That( _uut.Get("NotThere"), Is.EqualTo(null));
         }
