b062b8789d288883f28bcd13bf35cc70a8dff646
Dan Lee <au531980@uni.au.dk>
2017-03-27 13:58:40 +0200

    RedrawMoney

--- Common/Models/User.cs
+++ Common/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,6 +13,7 @@ namespace Common.Models
         private string _firstName;
         private string _lastName;
         private string _email;
+        private decimal _balance;
 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
@@ -44,7 +47,13 @@ namespace Common.Models
         }
 
         [Required]
-        public decimal Balance { get; set; }
+        public decimal Balance {
+            get { return _balance;  }
+            set
+            {
+                _balance = _balance + value;
+            }
+        }
 
         [Required]
         public string Hash { get; set; }
@@ -55,8 +64,16 @@ namespace Common.Models
         public virtual ICollection<Lobby> InvitedToLobbies { get; set; }
         public virtual ICollection<Bet> Bets { get; set; } 
         public virtual ICollection<Outcome> Outcomes { get; set; }
+        
 
-       
-
+        public decimal RedrawMoney(decimal amount)
+        {
+            if (Balance < amount)
+            {
+                throw new ArithmeticException(); 
+            }
+            Balance = Balance - amount;
+            return amount;
+        }
     }
 }
