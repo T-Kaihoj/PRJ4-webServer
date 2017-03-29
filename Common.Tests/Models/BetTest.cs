af07b0d4315627692bdc604d7d29d6c1d96096f8
HootERasmus <kominumsen@gmail.com>
2017-03-29 15:57:04 +0200

    Added UtilittCommen file

---
+++ Common.Tests/Models/BetTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Models;
+using Common.Tests;
+using NUnit.Framework;
+
+namespace Common.Tests.Models
+{
+    [TestFixture]
+    class BetTest
+    {
+        private Bet _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Bet();    
+        }
+
+        [Test]
+        public void Name_SetValidName_ValidNames()
+        {
+            foreach (var name in UtilityCommen.validNames)
+            {
+                _uut.Name = name;
+
+                Assert.That(_uut.Name, Is.EqualTo(name));
+            }
+        }
+
+        [Test]
+        public void Name_SetInvalidName_ThrowException()
+        {
+            
+        }
+    }
+}
---
+++ Common.Tests/Models/OutcomeTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Models;
+using NUnit.Framework;
+
+namespace Common.Tests.Models
+{
+    [TestFixture]
+    class OutcomeTest
+    {
+        private Outcome _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Outcome();
+        }
+    }
+}
