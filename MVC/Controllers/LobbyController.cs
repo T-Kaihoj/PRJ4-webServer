04dbae83d546b6c6d800b73feed23d4989929997
Niklas <niklasmms7@hotmail.com>
2017-03-30 14:30:57 +0200

    Accept lobby invite implemented in LobbyController.cs

--- MVC/Controllers/LobbyController.cs
+++ MVC/Controllers/LobbyController.cs
@@ -84,6 +84,21 @@ namespace MVC.Controllers
                 return View(viewModel);
             }   
         }
+        //GET: /<controller/Accept/<id>
+        public ActionResult Accept(long id)
+        {
+            using (var myWork = _factory.GetUOF())
+            {
+                var lobby = myWork.Lobby.Get(id);
+
+                if(lobby != null) 
+                    myWork.Lobby.Get(id).AcceptLobby(myWork.User.Get(_userContext.Identity.Name));
+            }
+
+
+            //TODO: More error handling?
+            return Redirect("Lobby/List");
+        }
 
         // GET: /<controller>/Show/<id>
         public ActionResult Show(long id)
