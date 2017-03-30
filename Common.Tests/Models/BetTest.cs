88788be289c6e105d2b0086502016d8f918d45af
HootERasmus <kominumsen@gmail.com>
2017-03-30 08:15:42 +0200

    BetTest

--- Common.Tests/Models/BetTest.cs
+++ Common.Tests/Models/BetTest.cs
@@ -24,24 +24,81 @@ namespace Common.Tests.Models
         }
 
         [Test]
-        public void Name_SetValidName_ValidCharacters()
+        public void OutcomeId_SetOutcomeId_OutcomeIdSet()
+        {
+            foreach (var id in UtilityCommen.ValidIds)
+            {
+                Assert.That(() => _uut.BetId = id, Throws.Nothing);
+            }
+        }
+
+        [Test]
+        public void OutcomeId_GetOutcomeId_OutComeIdReturned()
+        {
+            foreach (var id in UtilityCommen.ValidIds)
+            {
+                _uut.BetId = id;
+                Assert.That(_uut.BetId, Is.EqualTo(id));
+            }
+        }
+
+        [Test]
+        public void Name_SetValidName_ValidNameSet()
+        {
+            foreach (var chars in UtilityCommen.ValidCharacters)
+            {
+                Assert.That(() => _uut.Name = chars, Throws.Nothing);
+            }
+        }
+
+        [Test]
+        public void Name_GetName_NameReturened()
         {
             foreach (var name in UtilityCommen.ValidCharacters)
             {
                 _uut.Name = name;
-
                 Assert.That(_uut.Name, Is.EqualTo(name));
             }
         }
 
-        
         [Test]
-        public void Name_SetInvalidName_ThrowException()
+        public void Name_SetInvalidName_ThrowsException()
         {
-            foreach (var name in UtilityCommen.InvalidCharacters)
+            foreach (var chars in UtilityCommen.InvalidCharacters)
             {
-                Assert.That(() => _uut.Name = name, Throws.Exception);
+                Assert.That(() => _uut.Name = chars, Throws.Exception);
             }
         }
+
+
+
+        [Test]
+        public void Description_SetValidDescription_DescriptionSet()
+        {
+            foreach (var chars in UtilityCommen.ValidCharacters)
+            {
+                Assert.That(() => _uut.Description = chars, Throws.Nothing);
+            }
+        }
+
+        [Test]
+        public void Description_GetValidDescription_DescriptionReturned()
+        {
+            foreach (var chars in UtilityCommen.ValidCharacters)
+            {
+                _uut.Description = chars;
+                Assert.That(_uut.Description, Is.EqualTo(chars));
+            }
+        }
+
+        [Test]
+        public void Description_SetInvalidDescription_ThrowExecption()
+        {
+            foreach (var chars in UtilityCommen.InvalidCharacters)
+            {
+                Assert.That(() => _uut.Description = chars, Throws.Exception);
+            }
+        }
+
     }
 }
