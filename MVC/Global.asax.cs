f0eff4435c4973a37d2433a406a74a20d079c271
Niklas <niklasmms7@hotmail.com>
2017-04-04 11:15:44 +0200

    Removed unused code.

--- MVC/Global.asax.cs
+++ MVC/Global.asax.cs
@@ -22,16 +22,6 @@ namespace MVC
     [ExcludeFromCodeCoverage]
     public class MvcApplication : System.Web.HttpApplication
     {
-
-        /*
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.MapRoute(
-            "RemoveBet",                                              // Route name
-            "{controller}/{action}/{Lobby}/{Bet}",                           // URL with parameters
-            new { controller = "Bet", action = "Remove", Lobby = "", Bet = "" }  // Parameter defaults
-        );
-        }*/
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
