af97f53cff01cdf29264749b92fd2b7b7707209a
Dan Lee <au531980@uni.au.dk>
2017-05-03 09:09:39 +0200

    Withdraw, Deposit

--- MVC/ViewModels/Money/WithdrawViewModel.cs
+++ MVC/ViewModels/Money/WithdrawViewModel.cs
@@ -7,7 +7,7 @@ namespace MVC.ViewModels
     public class WithdrawViewModel
     {
         [Required(ErrorMessage = "Please provide amount you want to withdraw")]
-        [DisplayName("Amount to withdraw")]
+        [DisplayName("Amount to withdraw"), Range(1, 10000), DataType(DataType.Currency)]
         public decimal Withdraw { get; set; }
         public decimal CurrentBalance { get; set; }
        
