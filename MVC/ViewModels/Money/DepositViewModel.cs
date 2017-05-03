a27b5bf7097bcc8dc0dc80442cf834fa1149df13
Dan Lee <au531980@uni.au.dk>
2017-05-03 21:50:06 +0200

    fixes

--- MVC/ViewModels/Money/DepositViewModel.cs
+++ MVC/ViewModels/Money/DepositViewModel.cs
@@ -6,8 +6,8 @@ namespace MVC.ViewModels
     public class DepositViewModel
     {
         [Required(ErrorMessageResourceType = typeof(Resources.User),
-            ErrorMessageResourceName = "ErrorDepositRequired")]
-        [DisplayName("Amount to deposit")]
+           ErrorMessageResourceName = "ErrorDepositRequired")]
+        [DisplayName("Amount to deposit"), Range(1, 10000), DataType(DataType.Currency)]
         public decimal Deposit { get; set; }
         public decimal CurrentBalance { get; set; }
     }
--- MVC/ViewModels/Money/WithdrawViewModel.cs
+++ MVC/ViewModels/Money/WithdrawViewModel.cs
@@ -7,7 +7,7 @@ namespace MVC.ViewModels
     {
         [Required(ErrorMessageResourceType = typeof(Resources.User),
             ErrorMessageResourceName = "ErrorWithdrawRequired")]
-        [DisplayName("Amount to withdraw")]
+        [DisplayName("Amount to withdraw"), Range(1, 10000), DataType(DataType.Currency)]
         public decimal Withdraw { get; set; }
         public decimal CurrentBalance { get; set; }
     }
