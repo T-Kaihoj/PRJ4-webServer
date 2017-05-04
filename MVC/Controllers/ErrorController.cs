ae8d0ce33ff55b732e2a1a59776678828abc7914
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-05-04 17:02:25 +0200

    Add custom error pages.

---
+++ MVC/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            return View("~/Views/Shared/FileNotFound.cshtml");
+        }
+
+        public ActionResult NotPermitted()
+        {
+            return View("~/Views/Shared/UnauthorizedAccess.cshtml");
+        }
+
+        public ActionResult Index()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
