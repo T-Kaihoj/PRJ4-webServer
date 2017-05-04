f9446054ac516d1d5883b7e18fa2e6199d8845ab
Magnus Aagaard Sørensen <20053175@post.au.dk>
2017-05-04 17:07:29 +0200

    Fix custom errors.

--- MVC/Controllers/ErrorController.cs
+++ MVC/Controllers/ErrorController.cs
@@ -6,16 +6,22 @@ namespace MVC.Controllers
     {
         public ActionResult NotFound()
         {
+            Response.StatusCode = 404;
+
             return View("~/Views/Shared/FileNotFound.cshtml");
         }
 
         public ActionResult NotPermitted()
         {
+            Response.StatusCode = 403;
+
             return View("~/Views/Shared/UnauthorizedAccess.cshtml");
         }
 
         public ActionResult Index()
         {
+            Response.StatusCode = 500;
+
             return View("~/Views/Shared/Error.cshtml");
         }
     }
